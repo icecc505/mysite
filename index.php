e120d94750b04da46c406e53f10cdb9be5d33a04
icecc505 <releasedice@gmail.com>
2016-08-18 10:14:45 -0600

    added a bunch of stuff
    style.css, about.php, headUtils.php, index.php and lib

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+
+  <head>
+
+    <?php require_once("lib/headUtils.php");?>
+
+  </head>
+
+  <body>
+    <nav class="navbar navbar-default">
+          <div class="navbar-header">
+              <a class="navbar-brand" href="http://designplebs.com"> Design by Plebs
+                    </a>
+          <div class="row">
+              <div class="container">
+                  <div class="col-md-8 col-md-offset-1">
+                      <div class="collapse navbar-collapse pull-right">
+                          <ul class="nav navbar-nav">
+                            <li role="presentation"><a href="index.php"><i class="fa fa-home fa-3x"
+                               aria-hidden="true"></i></a></li>
+                            <li role="presentation"><a href="about.php">About</a></li>
+                            <li role="presentation"><a href="#">Contact</a></li>
+                            <i class="fa fa-facebook-square fa-3x" aria-hidden="true"></i>
+                          </ul>
+                  </div>
+              </div>
+          </div>
+        </div>
+      </nav>
+    </div>
+
+
+
+    <!--<ul class="nav nav-tabs fixed-top">
+
+      <li role="presentation"><a href="index.php"><i class="fa fa-home fa-5x" aria-hidden="true"></i></a></li>
+      <li role="presentation"><a href="about.php">About</a></li>
+      <li role="presentation"><a href="#">Contact</a></li>
+      <i class="fa fa-facebook-square fa-4x" aria-hidden="true"></i>
+    </ul> -->
+
+
+<div class = "container" id ="eyewear">
+  <!-- <img src="lib/images/wolf.jpg" alt="" /> -->
+
+    <h1>Design the pleb way</h1>
+  <!-- <a href="#goHere"><button type="button" name="button">Link Listen</button></a> -->
+  <!-- <div class="goHere"> -->
+
+  </div>
+</div>
+
+  </body>
+
+  <link rel="stylesheet" href="lib/css/style.css">
+
+</html>
